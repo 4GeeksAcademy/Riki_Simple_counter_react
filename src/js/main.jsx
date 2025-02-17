664569fabb9deef517dd3aa4a8957683ad96c722
RikiTeethdog <flanseros@yahoo.es>
2025-02-17 11:58:06 +0000

    RikiCounter

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-//Bootstrap
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
 
-// index.css'
-import '../styles/index.css'
+// index.css
+import '../styles/index.css';
 
-// components
-import Home from './components/Home';
+let seconds = 0;
 
+function updateCounter() {
+    seconds++;
+    document.getElementById("counter").innerText = `Los segundos son: ${seconds}`;
+}
+setInterval(updateCounter, 1000);
+
+function App() {
+  return (
+      <div className="App">
+          <h1 id="counter">Los segundos son: 0</h1>
+      </div>
+  );
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+      <App />
+  </React.StrictMode>
+);
+
